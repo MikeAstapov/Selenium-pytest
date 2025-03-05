9b0302e35914fa4df61bd60b911b160b45945df4
Astapov_M <astapovms@yandex.ru>
2025-03-05 14:23:36 +0300

    добавлены тесты методов Patch и PUT

--- test_restful_booker.py
+++ test_restful_booker.py
@@ -1,9 +1,8 @@
-from http.client import responses
 
 import requests
 import pytest
 
-from config import USERNAME, PASSWORD_booker, URL_BOOKS, AUTHORIZATION
+from config import USERNAME, PASSWORD_booker, URL_BOOKS, HEADERS
 
 
 @pytest.fixture()
@@ -19,11 +18,6 @@ def auth_func():
 
 @pytest.fixture()
 def create_book():
-    headers = {
-
-        "Authorization": AUTHORIZATION
-
-    }
     data = {
         "firstname": "Michael",
         "lastname": "Astapov",
@@ -37,14 +31,34 @@ def create_book():
     }
     response = requests.post("https://restful-booker.herokuapp.com/booking", json=data).json()
     yield response["bookingid"]
-    requests.delete(f"https://restful-booker.herokuapp.com/booking/{response['bookingid']}", headers=headers)
+    requests.delete(f"https://restful-booker.herokuapp.com/booking/{response['bookingid']}", headers=HEADERS)
 
 
 def test_get_booking_ids(create_book):
     response = requests.get(f"{URL_BOOKS}/booking/{create_book}").json()
-    print(create_book)
     assert response["firstname"] == "Michael" and response["lastname"] == "Astapov" and response[
         "totalprice"] == 999, f"Id {create_book} не найден"
 
-#def test_create_book(create_book):
 
+def test_update_book(create_book):
+    data = {
+        "firstname": "James",
+        "lastname": "Brown",
+        "totalprice": 111,
+        "depositpaid": True,
+        "bookingdates": {
+            "checkin": "2018-01-01",
+            "checkout": "2019-01-01"
+        },
+        "additionalneeds": "Breakfast"
+    }
+    response = requests.put(f"{URL_BOOKS}/booking/{create_book}", json=data, headers=HEADERS).json()
+    assert response['firstname'] == 'James' and response['lastname'] == 'Brown' and response[
+        'totalprice'] == 111, "Обновление данных вызвало ошибку"
+
+
+def test_patch_book(create_book):
+    data = {"firstname": "TEST NAME",
+            "lastname": "TEST SURNAME"}
+    response = requests.patch(f"{URL_BOOKS}/booking/{create_book}", json=data, headers=HEADERS).json()
+    assert response['firstname'] == 'TEST NAME' and response['lastname'] == 'TEST SURNAME', "Частичное обновление данных вызвало ошибку"
