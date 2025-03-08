266f0f4264186dc86f93126af22dfa65d9b1ab70
Astapov_M <astapovms@yandex.ru>
2025-03-08 15:31:17 +0300

    мелкие правки

--- conftest.py
+++ conftest.py
@@ -1,14 +1,6 @@
 import pytest
 import requests
 
-
-@pytest.fixture(scope="module")
-def create_book():
-    response = requests.post("https://restful-booker.herokuapp.com/booking", json=data_create_method).json()
-    yield response["bookingid"]
-    requests.delete(f"https://restful-booker.herokuapp.com/booking/{response['bookingid']}", headers=HEADERS)
-
-
 URL_BOOKS = "https://restful-booker.herokuapp.com"
 
 HEADERS = {
@@ -43,3 +35,10 @@ data_put_method = {
 
 data_patch_method = {"firstname": "TEST NAME",
                      "lastname": "TEST SURNAME"}
+
+
+@pytest.fixture(scope="module")
+def create_book():
+    response = requests.post("https://restful-booker.herokuapp.com/booking", json=data_create_method).json()
+    yield response["bookingid"]
+    requests.delete(f"https://restful-booker.herokuapp.com/booking/{response['bookingid']}", headers=HEADERS)
