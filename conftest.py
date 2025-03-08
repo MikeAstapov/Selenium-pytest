328f22310fb8e6ed607eb4a09660afb71d9db143
Astapov_M <astapovms@yandex.ru>
2025-03-08 15:23:43 +0300

    вынесение фикстуры в conftest

--- conftest.py
+++ conftest.py
@@ -1,3 +1,14 @@
+import pytest
+import requests
+
+
+@pytest.fixture(scope="module")
+def create_book():
+    response = requests.post("https://restful-booker.herokuapp.com/booking", json=data_create_method).json()
+    yield response["bookingid"]
+    requests.delete(f"https://restful-booker.herokuapp.com/booking/{response['bookingid']}", headers=HEADERS)
+
+
 URL_BOOKS = "https://restful-booker.herokuapp.com"
 
 HEADERS = {
