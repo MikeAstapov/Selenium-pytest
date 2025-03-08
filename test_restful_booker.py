667f722364439ed7ad70fedee1cb8e12e248bdae
Astapov_M <astapovms@yandex.ru>
2025-03-08 12:36:12 +0300

    добавлены негативные тесты для метода DELETE

--- test_restful_booker.py
+++ test_restful_booker.py
@@ -1,5 +1,3 @@
-from symtable import Class
-
 import requests
 import pytest
 
@@ -24,7 +22,14 @@ def create_book():
     requests.delete(f"https://restful-booker.herokuapp.com/booking/{response['bookingid']}", headers=HEADERS)
 
 
-class PostMethods():
+class TestGetMethods():
+    def test_get_booking_ids_positive(self, create_book):
+        response = requests.get(f"{URL_BOOKS}/booking/{create_book}").json()
+        assert response["firstname"] == "Michael" and response["lastname"] == "Astapov" and response[
+            "totalprice"] == 999, f"Id {create_book} не найден"
+
+
+class TestPostMethods():
 
     def test_auth_func_positive(self):
         data = {
@@ -39,7 +44,7 @@ class PostMethods():
             pytest.fail(f"Ошибка авторизации : {response.json()["reason"]}")
 
 
-class PutMethods():
+class TestPutMethods():
     def test_put_book_positive(self, create_book):
         data = {
             "firstname": "James",
@@ -57,7 +62,7 @@ class PutMethods():
             'totalprice'] == 111, "Обновление данных вызвало ошибку"
 
 
-class PatchMethods():
+class TestPatchMethods():
     def test_patch_book_positive(self, create_book):
         data = {"firstname": "TEST NAME",
                 "lastname": "TEST SURNAME"}
@@ -66,14 +71,19 @@ class PatchMethods():
             'lastname'] == 'TEST SURNAME', "Частичное обновление данных вызвало ошибку"
 
 
-class DeleteMethods():
-    def test_get_booking_ids_positive(self, create_book):
-        response = requests.get(f"{URL_BOOKS}/booking/{create_book}").json()
-        assert response["firstname"] == "Michael" and response["lastname"] == "Astapov" and response[
-            "totalprice"] == 999, f"Id {create_book} не найден"
+class TestDeleteMethods():
 
     def test_delete_book_positive(self, create_book):
         requests.delete(f"https://restful-booker.herokuapp.com/booking/{create_book}", headers=HEADERS)
-        print(create_book)
         response = requests.get(f"{URL_BOOKS}/booking/{create_book}")
         assert response.status_code == 404, f"Запись не была удалена. Id книги {create_book}"
+
+    def test_delete_book_negative(self, create_book):
+        # Тест без HEADERS, в которых есть bearer токен
+        responce = requests.delete(f"https://restful-booker.herokuapp.com/booking/{create_book}")
+        assert responce.status_code == 403, "Удалено без права доступа к методу"
+
+    def test_delete_book_negative(self, create_book):
+        # Тест удаление несуществующей записи с id=999999999999
+        responce = requests.delete(f"https://restful-booker.herokuapp.com/booking/999999999999", headers=HEADERS)
+        assert responce.status_code == 405, "Успешный ответ об удалении несуществующей записи"
